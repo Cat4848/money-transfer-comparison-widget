ebc5623013c6cd24cc891e7d3d74a56bdea9316c
Cat4848 <catalinrol@gmail.com>
2025-09-10 21:59:35 +0100

    feat: built the Option component

--- src/app/components/Option.tsx
+++ src/app/components/Option.tsx
@@ -1,9 +1,28 @@
 import { Provider } from "../lib/types";
 
 interface Props {
-  provider: Provider
+  provider: Provider;
 }
 
-export default function Option(props: Props) {
-  
+export default function Option({ provider }: Props) {
+  return (
+    <div>
+      <div>{provider.name}</div>
+      <div>{provider.type}</div>
+      {provider.quotes.map((quote, i) => (
+        <div key={`provider-quote-${i}`}>
+          <div>{quote.fee}</div>
+          <div>{quote.rate}</div>
+          <div>{quote.receivedAmount}</div>
+          {quote.deliveryEstimation.duration && (
+            <div>
+              {quote.deliveryEstimation.duration.max
+                ? quote.deliveryEstimation.duration.max
+                : quote.deliveryEstimation.duration.min}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
 }
