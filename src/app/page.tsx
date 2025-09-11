4ded87e3a728cf07db2e6925830b3a7f3e1a1514
Cat4848 <catalinrol@gmail.com>
2025-09-11 19:38:57 +0100

    feat: added the Option component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 import styles from "./page.module.css";
 import { useQuote } from "./hooks/useQuote";
+import Option from "./components/Option";
 
 export default function Home() {
   const { loading, error, quote } = useQuote();
+
+  if (loading) {
+    return <h3>loading...</h3>;
+  }
+  if (error) {
+    return <h3>{error}</h3>;
+  }
+
   return (
     <div className={styles.page}>
-      <h1>Home Page</h1>
+      <h1>Money Transfer Comparison</h1>
+
+      {quote &&
+        quote.providers.map((provider) => (
+          <Option key={provider.id} provider={provider} />
+        ))}
     </div>
   );
 }
